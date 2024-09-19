926c4cfcd242758483abd942577ff94701a29680
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-19 13:15:15 +0200

    test: capacity 1 zombies 0

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -3,11 +3,18 @@ import { test } from "node:test";
 
 const createRoom = (capacity: number) => {
   const _capacity = capacity;
-  const _numberOfZombies: number[]=[];
+  const _numberOfZombies: string[]=[];
   
   return {
-    isFull: () => _capacity===_numberOfZombies.length,
-    // addZombie: 
+    isFull: () => _capacity === _numberOfZombies.length,
+    addZombie: ()=>{
+      if(_capacity===0){
+        return;
+      };
+      if(_capacity===_numberOfZombies.length){
+        _numberOfZombies.shift();
+      };
+    }, 
   };
 };
 
@@ -19,7 +26,12 @@ test("room is full", () => {
   ok(result);
 });
 
-test.skip("empty room that fits one zombie is not full", () => {});
+test("empty room that fits one zombie is not full", () => {
+  const room = createRoom(1);
+
+  const result = room.isFull();
+  ok(!result);
+});
 
 test.skip("empty room cannot fit any zombies", () => {});
 
